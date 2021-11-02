e8af2728e6dd1327e40017e0559f712cda9ee9c2
Alex Bailey <baileymk@hotmail.com>
2021-11-02 17:40:53 +0000

    first commit

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ for (let i = 0; i < drums.length; i++) {
     drums[i].addEventListener("click", function () {
         // playsound(this.id)
         new Audio("sounds/" + this.id + ".wav").play();
+
         drums[i].style.background = "#5a189a"
         setTimeout(function () {
             drums[i].style.background = "#240046"
@@ -24,12 +25,14 @@ document.addEventListener("keydown", function (event) {
     for (let i = 0; i < drums.length; i++) {
         if (drums[i].classList.contains(event.key)) {
             new Audio("sounds/" + drums[i].id + ".wav").play();
+
             let id = drums[i].id
             drums[id-1].style.background = "#5a189a"
             console.log(drums[i].id);
             setTimeout(function () {
                 drums[id-1].style.background = "#240046"
             }, 30)
+            
             drums[id-1].style.transform = "scale(0.98)"
             setTimeout(function () {
                 drums[id-1].style.transform = "scale(1)"
