a5841f011d545581748a220f710c92656602f7a1
Alex Bailey <baileymk@hotmail.com>
2021-11-02 19:18:34 +0000

    changed styling

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 //@ts-check
 //Arrow functions do not have their own bindings for this, resulting in this retaining the value of the enclosing lexical context's this.
 //We are using a regular function, so the this context inside the callback will be bound to the event target.
-
+let btm = document.querySelectorAll(".btm")
 let drums = document.querySelectorAll(".drum")
 
 for (let i = 0; i < drums.length; i++) {
     drums[i].addEventListener("click", function () {
-        // playsound(this.id)
         new Audio("sounds/" + this.id + ".wav").play();
 
-        drums[i].style.background = "#5a189a"
+        drums[i].style.transform = "translateY(10px)"
         setTimeout(function () {
-            drums[i].style.background = "#240046"
+            drums[i].style.transform = "translateY(0px)"
+        }, 30);
+        drums[i].style.height = 90 + "px"
+        setTimeout(function () {
+            drums[i].style.height = 100 + "px"
+        }, 30);
+        
+        btm[i].style.display = "none"
+        setTimeout(function () {
+            btm[i].style.display = "block"
         }, 30);
     });
 }
@@ -21,17 +29,21 @@ document.addEventListener("keydown", function (event) {
         if (drums[i].classList.contains(event.key)) {
             new Audio("sounds/" + drums[i].id + ".wav").play();
 
-            let id = drums[i].id
-            drums[id-1].style.background = "#5a189a"
+            drums[i].style.transform = "translateY(10px)"
             console.log(drums[i].id);
             setTimeout(function () {
-                drums[id-1].style.background = "#240046"
-            }, 30)
 
-            drums[id-1].style.transform = "scale(0.98)"
+                drums[i].style.transform = "translateY(0px)"
+            }, 30);
+            drums[i].style.height = 90 + "px"
             setTimeout(function () {
-                drums[id-1].style.transform = "scale(1)"
-            }, 50)
+                drums[i].style.height = 100 + "px"
+            }, 30);
+            
+            btm[i].style.display = "none"
+            setTimeout(function () {
+                btm[i].style.display = "block"
+            }, 30);
         }     
     }
 })
